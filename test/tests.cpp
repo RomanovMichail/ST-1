6b3e049c7d9b9d4dee2c1aec1a6891f24c778887
RomanovMichail <144445013+RomanovMichail@users.noreply.github.com>
2026-03-01 11:22:34 +0300

    fix5

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,10 +1,8 @@
 // Copyright 2025 UNN-CS
 
-#include "alg.h"
-
-#include <cstdint>
-
 #include <gtest/gtest.h>
+#include <cstdint>
+#include "alg.h"
 
 TEST(PrimeValidation, SmallValues) {
   EXPECT_FALSE(checkPrime(0));
